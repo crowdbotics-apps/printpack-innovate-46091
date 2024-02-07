7d20f13850712bfd155258701e8282d8a4860c78
Crowdbotics <team@crowdbotics.com>
2024-02-07 17:19:08 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "printpack-innovate-46091",
+  url: "https://printpack-innovate-46091.botics.co",
+  api: "https://printpack-innovate-46091.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
